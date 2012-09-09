e881a2c1e393e8e5c2eca506a9c21ab8d02442f5
Benjamin Gudehus <hastebrot@gmail.com>
2012-09-10 01:31:36 +0200

    Added; command line arguments for the data seeder.

--- README.md
+++ README.md
@@ -6,8 +6,21 @@
 
     $ cd devchan
     $ npm install
-    $ node seeds
+    npm http GET https://registry.npmjs.org/superagent/0.9.1
+    npm http GET https://registry.npmjs.org/mocha/1.4.2
+    npm http GET https://registry.npmjs.org/marked/0.2.5
+    ...
+
+    $ node seeds Board boards.yml
+    Seeding model 'Board' with data from 'boards.yml'...
+    { name: 'prog', description: 'prog' }
+    { name: 'foo', description: 'foo' }
+    { name: 'bar', description: 'bar' }
+    { name: 'baz', description: 'baz' }
+
     $ node server
+    Connected to database on port 27017
+    Express server listening on port 3789
 
 ## Development
 
--- seeds.js
+++ seeds.js
@@ -1,6 +1,6 @@
-//-----------------------------------------------------------------------------
+//-------------------------------------
 // MODULE DEPENDENCIES.
-//-----------------------------------------------------------------------------
+//-------------------------------------
 
 var fs = require("fs")
 var mongoose = require("mongoose")
@@ -9,28 +9,44 @@ var _ = require("underscore")
 var async = require("async")
 var yaml = require("js-yaml")
 
-//-----------------------------------------------------------------------------
-// DATA SEEDER.
-//-----------------------------------------------------------------------------
-
 var Devchan = require("./app/index")
+var fixtures = fs.readdirSync("./data/fixtures")
+fixtures = _.select(fixtures, function(fixtureFilename) { return fixtureFilename[0] != "_" })
 
-var fixtures = fs.readdirSync("./data/fixture")
-var models = Devchan.db.models
-console.log("models:", _.keys(Devchan.db.models))
-console.log("fixtures:", fixtures)
+//-------------------------------------
+// COMMAND LINE ARGUMENTS AND USAGE.
+//-------------------------------------
 
-Devchan.db.connect(function(err) {
-  if (err) throw err
+var modelName = process.argv[2]
+var fixtureFilename = process.argv[3]
 
-  var boards = require("./data/fixture/boards-static.yml")
-  async.forEach(boards, function(board, callback) {
-    console.log(board)
-    models.Board(board).save(callback)
-  }, function(err) {
-    console.log(err)
-    Devchan.db.disconnect(function() {
-      process.exit(0)
+if (!modelName || !fixtureFilename) {
+  console.log("Usage:\n\tnode seeds <modelName> <fixtureFilename>\n")
+  console.log("<modelName>:\n\t" + _.keys(Devchan.db.models).join(", ") + "\n")
+  console.log("<fixtureFilename>:\n\t" + fixtures.join(", ") + "\n")
+  process.exit(0)
+}
+
+//-------------------------------------
+// DATA SEEDER.
+//-------------------------------------
+
+if (module === require.main) {
+  console.log("Seeding model '" + modelName + "' with data from '" + fixtureFilename + "'...")
+
+  Devchan.db.connect(function(err) {
+    if (err) throw err
+
+    var modelObject = Devchan.db.models[modelName]
+    var fixtureData = require("./data/fixtures/" + fixtureFilename)
+    async.forEach(fixtureData, function(dataset, callback) {
+      console.log(dataset)
+      new modelObject(dataset).save(callback)
+    }, function(err) {
+      Devchan.db.disconnect(function() {
+        if (err) throw err
+        process.exit(0)
+      })
     })
   })
-})
+}
